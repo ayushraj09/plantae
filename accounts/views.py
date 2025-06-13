420793ee2e1a06319cef3474334f15deb1a666fd
Ayush Raj <ayush.ttps@gmail.com>
2025-06-14 00:42:02 +0530

    Buttons fixed for logged in user

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
+import requests
 
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -57,9 +60,62 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exist = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exist:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    
+                    #getting product variation by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    #get cart items from user to access his product variation
+                    cart_item = CartItem.objects.filter(user = user)
+                    ex_var_list=[]
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # let product_variation = [1, 2, 3, 4, 6]
+                    # and ex_var_list=[4, 6, 3, 5]
+                    # now we have to check common in both list
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id = item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+
+            except:
+                pass
             auth.login(request, user)
             # messsages.success(request, 'You are now logged in.')
-            return redirect('home')
+
+            # redirecting to next page
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # query = next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                # params = {'next': '/cart/checkout/'}
+                if 'next' in params:
+                    nextpage = params['next']
+                    return redirect(nextpage)
+                
+            except:
+                return redirect('home')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
