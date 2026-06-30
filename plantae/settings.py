3295f093a72902d64de3a4893fb77d9f5a256540
Ayush Raj <ayush.ttps@gmail.com>
2026-06-30 12:00:27 +0530

    updated domain name

--- plantae/settings.py
+++ plantae/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['www.plantae.live', 'plantae.live', '20.193.248.79']
+ALLOWED_HOSTS = ['www.plantaeai.tech', 'plantaeai.tech', '20.193.248.79']
 
 
 # Application definition
