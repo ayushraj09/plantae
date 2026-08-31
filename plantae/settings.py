7624dc8dc3a321f3674fe2f7378289ee798697ad
Ayush Raj <ayush.ttps@gmail.com>
2026-08-31 20:17:55 +0530

    fixed

--- plantae/settings.py
+++ plantae/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
